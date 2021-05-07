45a4ae4c34d38bcb41a2a6000f484f69d81017ae
Amy (lydocia) <git@lydocia.com>
2021-05-07 13:39:26 +0200

    Discord.NET setup

---
+++ Bot Bucket/BotConfig.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot_Bucket {
+    public class BotConfig {
+        [JsonProperty("DiscordToken")]
+        public string Token { get; private set; }
+    }
+}
--- Bot Bucket/Program.cs
+++ Bot Bucket/Program.cs
@@ -1,9 +1,36 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Bot_Bucket {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+	public class Program {
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync() {
+            DiscordSocketClient _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            var token = JsonConvert.DeserializeObject<BotConfig>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
-    }
+        private Task Log(LogMessage msg) {
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
 }
