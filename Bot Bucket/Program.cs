25e0b71641dd540237397c01fa57194bb28e8abc
krisboyce <kris@krisboyce.com>
2021-09-29 22:35:37 -0500

    Call to install commands on startup

--- Bot Bucket/Program.cs
+++ Bot Bucket/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using System;
@@ -20,9 +21,20 @@ namespace Bot_Bucket {
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
+            var config = new CommandServiceConfig();
+            if (File.Exists("command-config.json"))
+            {
+                await Log(new LogMessage(LogSeverity.Info, "Startup", "Loading configuration from file"));
+                config = JsonConvert.DeserializeObject<CommandServiceConfig>(File.ReadAllText("command-config.json"));
+            }
+            
+            var handler = new CommandHandler(_client, new CommandService(config));
+            await handler.InstallCommandsAsync();
+
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
+
         private Task Log(LogMessage msg) {
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
